1ef555b3653cdad9ca9b70b434a9044347a04863
Joshua Sealand <jsealand@hmc.edu>
2018-06-08 22:06:03 +0000

    logging digital read working

--- alarmlogger.py
+++ alarmlogger.py
@@ -6,15 +6,29 @@ Logs to a csv file every 10 seconds
 
 import csv
 from datetime import datetime
+import time
+import os
+import RPi.GPIO as GPIO
 
 start = datetime.now()
 print(start)
-filename = str(start)[:19]+'_alarmlog.csv'
+filepath = '/media/pi/BC67-8823/CClogger/'
+filename = str(start)[:19].replace(':','')+'_alarmlog.csv'
 print(filename)
 
-with open(filename, 'wb') as csvfile:
+pin = 4
+# set GPIO mode to BCM numbering (GPIO4 is pin 7 on header)
+GPIO.setmode(GPIO.BCM)
+# set pin to input with internal pull up
+GPIO.setup(pin, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+
+with open(os.path.join(filepath, filename), 'wb') as csvfile:
     alarmwriter = csv.writer(csvfile)
 
+    # write header
+    alarmwriter.writerow(['Date', 'State'])
+    # write data
     while True:
-        data = [datetime.now(), 'hi']
+        data = [datetime.now(), GPIO.input(pin)]
         alarmwriter.writerow(data)
+	time.sleep(1)
