4f3840c61898a0ebb29e25ae4c5dc97be6984a60
landseal <sealandjoshua@gmail.com>
2018-06-27 12:24:31 -0700

    adding length of fault to alarmfinder

--- alarmfinder.py
+++ alarmfinder.py
@@ -9,11 +9,15 @@ alarms = pd.DataFrame()
 for root, dirs, files in os.walk('C:\\Users\\jsealand\\Documents\\ccalarms'):
     for i in files:
         if i[-4:] == '.csv':
-            print(i)
-            df = pd.read_csv(i, index_col=0)
-            df = df.dropna(axis=0) # drop Nan rows
-            df.index = pd.DatetimeIndex(df.index) # convert index to datetime
-            alarms = alarms.append(df)
+            try:
+                print(i)
+                df = pd.read_csv(i, index_col=0)
+                df = df.dropna(axis=0) # drop Nan rows
+                df.index = pd.DatetimeIndex(df.index) # convert index to datetime
+                alarms = alarms.append(df)
+            except pd.errors.EmptyDataError:
+                print(i, ' is empty')
+
 
 # open alarm logs file
 f = open('alarmlogs.txt', 'w')
@@ -21,8 +25,12 @@ f = open('alarmlogs.txt', 'w')
 # go through the state column and find all times when it goes from 1 -> 0
 for i in range(1, len(alarms.State)):
     if alarms.State.iloc[i-1] == 1 and alarms.State.iloc[i] == 0:
-        print('alarm at ', alarms.index[i])
-        f.write(str(alarms.index[i])+'\n')
+        for j in range(1, len(alarms.State) - i):
+            if alarms.State.iloc[i+j] == 1:
+                print('alarm at ', alarms.index[i], ' for ', end='')
+                print(alarms.index[i+j]-alarms.index[i], ' seconds')
+                f.write(str(alarms.index[i])+'\n')
+                break
 
 # close alarm logs file
 f.close()
